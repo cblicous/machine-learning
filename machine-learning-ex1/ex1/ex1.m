cee2929640234ed94d8e6ec8444fac29ade807e1
Christian Belka <Christian.Belka@mgm-tp.com>
2016-12-28 13:12:55 +0100

    fist steps

--- machine-learning-ex1/ex1/ex1.m
+++ machine-learning-ex1/ex1/ex1.m
@@ -39,12 +39,16 @@ pause;
 %% ======================= Part 2: Plotting =======================
 fprintf('Plotting Data ...\n')
 data = load('ex1data1.txt');
-X = data(:, 1); y = data(:, 2);
+X = data(:, 1);
+y = data(:, 2);
 m = length(y); % number of training examples
 
 % Plot Data
 % Note: You have to complete the code in plotData.m
-plotData(X, y);
+plot(X, y, 'rx', 'MarkerSize', 10);
+
+ylabel('Profit in $10,000s'); % Set the y−axis label 
+xlabel('Population of City in 10,000s'); % Set the x−axis label
 
 fprintf('Program paused. Press enter to continue.\n');
 pause;
